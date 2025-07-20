4062a8ff9567d07f9f508c4d16b0cd885c04bf82
Deepak <deepaksaraswat8913@gmail.com>
2025-07-20 14:39:28 +0530

    new array question

---
+++ Strings/intintostring.java
@@ -0,0 +1,15 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class intintostring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter  a number");
+        int a= sc.nextInt();
+        String s ="";
+        s+=a;
+        System.out.println(s);
+
+    }
+}
---
+++ Strings/plusoperater.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class plusoperater {
+    public static void main(String[] args) {
+        String s= "abc";
+        s=s+"abxc";
+        System.out.println(s);
+        System.out.println("abc"+10+20);
+        System.out.println(20+30+"abxc");
+
+    }
+}
