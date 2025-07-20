c22512f698372eb729590ea19996873de45f6fc4
Deepak <deepaksaraswat8913@gmail.com>
2025-07-20 13:14:07 +0530

    new array question

---
+++ Strings/containsandstartwith.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class containsandstartwith {
+    public static void main(String[] args) {
+        String s = "Physics Wallah";
+        System.out.println(s.contains("lla"));
+        System.out.println(s.startsWith("Phy"));
+        System.out.println(s.endsWith("ah"));
+    }
+}
---
+++ Strings/tolowercaseandconcatenation.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class tolowercaseandconcatenation {
+    public static void main(String[] args) {
+        String s ="Raghav is ainstructer";
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+        String a= "abc";
+        String b ="def";
+        System.out.println(a.concat(b));
+    }
+}
