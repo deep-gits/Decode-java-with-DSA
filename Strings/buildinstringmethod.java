8188861cd5a4a0b80303b7f75945a4522b3a0951
Deepak <deepaksaraswat8913@gmail.com>
2025-07-20 12:28:57 +0530

    new array question

---
+++ Strings/buildinstringmethod.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class buildinstringmethod {
+    public static void main(String[] args) {
+        String   s = "deepak saraswat";
+        System.out.println(s.charAt(8));
+        System.out.println(s.length());
+    }
+}
