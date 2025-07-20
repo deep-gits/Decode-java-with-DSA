b70bbf3f010d6049eee1f56c8c3fe11d5cf46d99
Deepak <deepaksaraswat8913@gmail.com>
2025-07-20 12:28:38 +0530

    new array question

---
+++ Strings/basics.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class basics {
+    public static void main(String[] args) {
+        Scanner sc  =new Scanner(System.in);
+        System.out.println("enter your sentence");
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+
+}
