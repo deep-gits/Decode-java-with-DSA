da0f8dd102e4469698260e706070eee7c2f93654
Deepak <deepaksaraswat8913@gmail.com>
2025-07-20 13:04:19 +0530

    new array question

---
+++ Strings/indexof.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class indexof {
+    public static void main(String[] args) {
+        String s = "Raghav";
+        System.out.println(s.indexOf('z'));
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.lastIndexOf('a'));
+        String a="abc";
+        String b ="dbc";
+        System.out.println(a.compareTo(b));
+
+    }
+}
