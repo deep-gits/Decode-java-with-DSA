2dc5dab462721464a4dbfc8831945d2a80c70d63
Deepak <deepaksaraswat8913@gmail.com>
2025-07-24 12:08:39 +0530

    DSA

---
+++ Strings/stringbuilderfunction.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class stringbuilderfunction {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("raghav");
+        StringBuilder tb = new StringBuilder("deepaik");
+        System.out.println(sb.compareTo(tb));
+    }
+}
