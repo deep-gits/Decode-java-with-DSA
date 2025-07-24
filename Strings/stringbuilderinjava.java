bf86fa3a5c6eb623308e8e05f6178ae699c1c18e
Deepak <deepaksaraswat8913@gmail.com>
2025-07-24 12:09:45 +0530

    DSA

---
+++ Strings/stringbuilderinjava.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class stringbuilderinjava {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("abc");
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+    }
+}
