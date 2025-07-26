2a9938c6afa86b7b38873dbe9838f86105fae21a
Deepak <deepaksaraswat8913@gmail.com>
2025-07-26 18:35:59 +0530

    DSA

---
+++ Strings/reverseeachwordinagivensentence.java
@@ -0,0 +1,34 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class reverseeachwordinagivensentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder s =new StringBuilder(sc.nextLine());
+        int i =0;
+        int j = 0;
+        int n = s.length();
+        while(j<n){
+            if(s.charAt(j)!=' ') j++;
+            else {
+                reverse(s,i,j-1);
+                i=j+1;
+                j=i;
+            }
+        }
+        reverse(s,i,j-1);
+        System.out.println(s);
+
+
+    }
+    public static void reverse(StringBuilder s,int i,int j){
+        while(i<=j){
+            char temp = s.charAt(i);
+            s.setCharAt(i,s.charAt(j));
+            s.setCharAt(j,temp);
+            i++;
+            j--;
+        }
+    }
+}
