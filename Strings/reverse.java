3e99a2e899f49e1408e12fea6deeeec96dc09abf
Deepak <deepaksaraswat8913@gmail.com>
2025-07-26 16:32:55 +0530

    DSA

---
+++ Strings/reverse.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class reverse {
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder("wednesday");
+        s.reverse();
+        System.out.println(s);
+    }
+}
