913d853f6e076acd2851bc971793a3fd255e976b
deep-gits <deepaksaraswat8913@gmail.com>
2026-03-09 20:47:51 +0530

    DSA

---
+++ Oops/StaticKeyword.java
@@ -0,0 +1,21 @@
+package Oops;
+
+public class StaticKeyword {
+    public static void main(String[] args) {
+
+    }
+}
+
+class studet{
+    String name;
+    int roll;
+    static String schoolname;
+
+    void setName(String name){
+        this.name=name;
+    }
+    String getname(String name){
+        return this.name;
+    }
+
+}
