ae70ceb94ab9d82d82373304602d138e473fd098
deep-gits <deepaksaraswat8913@gmail.com>
2026-03-24 16:18:21 +0530

    DSA

--- Array/BuyAndSellStocks.java
+++ Array/BuyAndSellStocks.java
@@ -1,4 +1,25 @@
 package Array;
 
 public class BuyAndSellStocks {
+    public static int buyandselstocks(int prices[]){
+        int buyprice =Integer.MAX_VALUE;
+        int maxProfit=0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyprice<prices[i]){
+                int profit=prices[i]-buyprice;
+                maxProfit=Math.max(maxProfit,profit);
+            }else {
+                buyprice=prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+
+        System.out.println(buyandselstocks(prices));
+    }
 }
