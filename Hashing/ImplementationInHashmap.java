09645d4c295940726e7810bbf1adaca251adeadf
deep-gits <deepaksaraswat8913@gmail.com>
2026-03-24 16:02:26 +0530

    DSA

--- Hashing/ImplementationInHashmap.java
+++ Hashing/ImplementationInHashmap.java
@@ -1,4 +1,32 @@
 package Hashing;
-
+import java.util.*;
 public class ImplementationInHashmap {
+    static class HashMap<K,V>{
+        private class Node{
+            K key;
+            V value;
+
+            public Node(K key,V value){
+                this.key=key;
+                this.value=value;
+            }
+        }
+
+        private int size;
+        private LinkedList<Node> buckets[];
+
+        @SuppressWarnings("unchecked")
+        public HashMap(){
+            this.size=0;
+            this.buckets=new LinkedList[4];
+            for (int i = 0; i < 4; i++) {
+                this.buckets[i]=new LinkedList<>();
+            }
+        }
+        
+        public void put(K key,V value){
+        }
+
+
+    }
 }
