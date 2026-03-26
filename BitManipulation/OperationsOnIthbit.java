65921a8c047f6f35c218ff84d9f91d1f68c6856d
deep-gits <deepaksaraswat8913@gmail.com>
2026-03-26 17:27:02 +0530

    DSA

--- BitManipulation/OperationsOnIthbit.java
+++ BitManipulation/OperationsOnIthbit.java
@@ -18,11 +18,25 @@ public class OperationsOnIthbit {
         return n & bitmask;
     }
 
+    public static int updateIthBit(int n,int i ,int newbit ){
+        n=clearIthbit(n,i);
+        int bitmask=newbit<<i;
+        return n|bitmask;
+
+//        if (newbit==0){
+//            return clearIthbit(n,i);
+//        }
+//        else {
+//            return setIthBit(n,i);
+//        }
+    }
+
 
     public static void main(String[] args) {
         System.out.println(getIthBit(10,2));
         System.out.println(setIthBit(10,2));
         System.out.println(clearIthbit(10,2));
+        System.out.println(updateIthBit(10,2,1));
 
     }
 }
