c032b63f80f337b36ed51cece6cd9374cae971e6
deep-gits <deepaksaraswat8913@gmail.com>
2026-06-02 15:10:48 +0530

    DSA

---
+++ Array/majorityelements.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class majorityelements {
+    public static int majorityelment(int[] num){
+        int count=0;
+        int candidate=0;
+        for (int nums = num){
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] num={2,3,2,4,5,6,3,2};
+        System.out.println(majorityelment(num));
+    }
+}
